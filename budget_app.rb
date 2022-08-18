c36f773855e707bfb6c6c8a572a60690371f18d0
Jess Goodlett <jessgoodlett@gmail.com>
2022-08-17 22:51:49 -0700

    Add function and styling to income page

--- budget_app.rb
+++ budget_app.rb
@@ -2,10 +2,23 @@ require 'sinatra'
 require "sinatra/content_for"
 require "tilt/erubis"
 
+require_relative "database_persistance"
+
 configure(:development) do
   require "sinatra/reloader"
 end
 
+before do
+  @storage = DatabasePersistance.new
+  @user = 1
+end
+
 get "/" do
   erb :user_dashboard, layout: :layout
+end
+
+get "/income" do
+  @income = @storage.list_user_income(@user)
+  @total_income = @storage.calculate_income(@user)
+  erb :income, layout: :layout
 end
---
+++ database_persistance.rb
@@ -0,0 +1,45 @@
+require "pg"
+
+class DatabasePersistance
+  def initialize
+    @db = PG.connect(dbname: "budget")
+  end
+
+  def list_user_income(user_id)
+    sql = "SELECT * FROM income WHERE user_id = $1"
+    result = query(sql, user_id)
+
+    result.map do |tuple|
+      { name: tuple["name"],
+        description: tuple["description"],
+        monthly: add_commas(tuple["monthly_amount"]),
+        yearly: add_commas((tuple["monthly_amount"].to_i * 12).to_s) }
+    end
+  end
+
+  def calculate_income(user_id)
+    sql = <<~SQL
+      SELECT SUM(monthly_amount) AS total_month,
+        SUM(monthly_amount * 12) AS total_year
+      FROM income 
+      WHERE user_id = $1
+      GROUP BY user_id
+    SQL
+    tuple = query(sql, user_id).first
+
+    total_month = add_commas(tuple["total_month"])
+    total_year = add_commas(tuple["total_year"])
+
+    { month: total_month, year: total_year}
+  end
+
+  private
+
+  def query(statement, *params)
+    @db.exec_params(statement, params)
+  end
+
+  def add_commas(num)
+    num.reverse.scan(/\d{3}|.+/).join(",").reverse
+  end
+end
