aee40011d4ec8b7135c26629785cf418bc18fd61
Jess Goodlett <jessgoodlett@gmail.com>
2022-08-17 22:51:10 -0700

    Update database tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,15 @@
-CREATE TABLE user (
+/* for development, to reset database */
+DROP TABLE IF EXISTS public.user;
+DROP TABLE IF EXISTS public.income;
+DROP TABLE IF EXISTS public.expense_category;
+DROP TABLE IF EXISTS public.expense;
+DROP TABLE IF EXISTS public.spending;
+DROP TABLE IF EXISTS public.saving_category;
+DROP TABLE IF EXISTS public.saving_account;
+DROP TABLE IF EXISTS public.debt_category;
+DROP TABLE IF EXISTS public.debt;
+
+CREATE TABLE app_user (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
@@ -10,15 +21,22 @@ CREATE TABLE user (
 
 CREATE TABLE income (
   name TEXT NOT NULL,
+  memo TEXT,
   monthly_amount REAL NOT NULL,
-  user_id INTEGER REFERENCES user(id)
+  duration_months INTEGER NOT NULL,
+  user_id INTEGER REFERENCES "user"(id)
+);
+
+CREATE TABLE expense_category (
+  id SERIAL PRIMARY KEY,
+  category_type TEXT NOT NULL
 );
 
 CREATE TABLE expense (
   name TEXT NOT NULL,
   amount_spent REAL NOT NULL,
   required BOOLEAN DEFAULT true,
-  user_id INTEGER REFERENCES user(id),
+  user_id INTEGER REFERENCES "user"(id),
   category_id INTEGER REFERENCES expense_category(id)
 );
 
@@ -28,7 +46,7 @@ CREATE TABLE spending (
   expense_category_id INTEGER REFERENCES expense_category(id)
 );
 
-CREATE TABLE expense_category (
+CREATE TABLE saving_category (
   id SERIAL PRIMARY KEY,
   category_type TEXT NOT NULL
 );
@@ -36,11 +54,11 @@ CREATE TABLE expense_category (
 CREATE TABLE saving_account (
   name SERIAL PRIMARY KEY,
   amount_saved REAL NOT NULL,
-  user_id INTEGER REFERENCES user(id),
+  user_id INTEGER REFERENCES "user"(id),
   category_id INTEGER REFERENCES saving_category(id)
 );
 
-CREATE TABLE saving_category (
+CREATE TABLE debt_category (
   id SERIAL PRIMARY KEY,
   category_type TEXT NOT NULL
 );
@@ -49,11 +67,13 @@ CREATE TABLE debt (
   name TEXT NOT NULL,
   amount REAL NOT NULL,
   interest_rate NUMERIC(6, 4),
-  user_id INTEGER REFERENCES user(id),
+  user_id INTEGER REFERENCES "user"(id),
   category_id INTEGER REFERENCES debt_category(id)
 );
 
-CREATE TABLE debt_category (
-  id SERIAL PRIMARY KEY,
-  category_type TEXT NOT NULL
-);
+/* Data for development -- User and income */
+INSERT INTO "user" (first_name, last_name, username, password, email) 
+  VALUES ('Jane', 'Doe', 'toobroke', 'secret', 'janedoe@broke.com');
+
+INSERT INTO income VALUES ('software engineer', 'full-time position' 5000, 12, 1);
+INSERT INTO income VALUES ('freelance developer', 'contract work. estimated yearly.' 2000, 12, 1);
