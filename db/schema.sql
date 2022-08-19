b37dfe0a0f9339212b4e1a57d5dc1dc229688216
Jess Goodlett <jessgoodlett@gmail.com>
2022-08-18 23:03:03 -0700

    Update sql statements

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
 /* for development, to reset database */
-DROP TABLE IF EXISTS public.user;
 DROP TABLE IF EXISTS public.income;
-DROP TABLE IF EXISTS public.expense_category;
 DROP TABLE IF EXISTS public.expense;
 DROP TABLE IF EXISTS public.spending;
-DROP TABLE IF EXISTS public.saving_category;
+DROP TABLE IF EXISTS public.expense_category;
 DROP TABLE IF EXISTS public.saving_account;
-DROP TABLE IF EXISTS public.debt_category;
+DROP TABLE IF EXISTS public.saving_category;
 DROP TABLE IF EXISTS public.debt;
+DROP TABLE IF EXISTS public.debt_category;
+DROP TABLE IF EXISTS public.app_user;
 
 CREATE TABLE app_user (
   id SERIAL PRIMARY KEY,
@@ -24,7 +24,7 @@ CREATE TABLE income (
   memo TEXT,
   monthly_amount REAL NOT NULL,
   duration_months INTEGER NOT NULL,
-  user_id INTEGER REFERENCES "user"(id)
+  user_id INTEGER REFERENCES app_user(id)
 );
 
 CREATE TABLE expense_category (
@@ -36,7 +36,7 @@ CREATE TABLE expense (
   name TEXT NOT NULL,
   amount_spent REAL NOT NULL,
   required BOOLEAN DEFAULT true,
-  user_id INTEGER REFERENCES "user"(id),
+  user_id INTEGER REFERENCES app_user(id),
   category_id INTEGER REFERENCES expense_category(id)
 );
 
@@ -54,7 +54,7 @@ CREATE TABLE saving_category (
 CREATE TABLE saving_account (
   name SERIAL PRIMARY KEY,
   amount_saved REAL NOT NULL,
-  user_id INTEGER REFERENCES "user"(id),
+  user_id INTEGER REFERENCES app_user(id),
   category_id INTEGER REFERENCES saving_category(id)
 );
 
@@ -67,13 +67,13 @@ CREATE TABLE debt (
   name TEXT NOT NULL,
   amount REAL NOT NULL,
   interest_rate NUMERIC(6, 4),
-  user_id INTEGER REFERENCES "user"(id),
+  user_id INTEGER REFERENCES app_user(id),
   category_id INTEGER REFERENCES debt_category(id)
 );
 
 /* Data for development -- User and income */
-INSERT INTO "user" (first_name, last_name, username, password, email) 
+INSERT INTO app_user (first_name, last_name, username, password, email) 
   VALUES ('Jane', 'Doe', 'toobroke', 'secret', 'janedoe@broke.com');
 
-INSERT INTO income VALUES ('software engineer', 'full-time position' 5000, 12, 1);
-INSERT INTO income VALUES ('freelance developer', 'contract work. estimated yearly.' 2000, 12, 1);
+INSERT INTO income VALUES ('software engineer', 'full-time position', 5000, 12, 1);
+INSERT INTO income VALUES ('freelance developer', 'contract work. estimated yearly.', 2000, 12, 1);
